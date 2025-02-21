914260cd5a5da0c8dc032285caa1655743531b64
KauanneL <kauanne.l@escolar.ifrn.edu.br>
2025-02-21 15:13:37 -0300

    mod

--- src/controller/PacienteController.ts
+++ src/controller/PacienteController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import * as PacienteModel from '../modelos/pacienteModelo';
+import * as PacienteModel from '../modelos/pacientemodelo';
 
 // Função para obter os alunos
 export async function getPaciente(req: Request, res: Response) {
