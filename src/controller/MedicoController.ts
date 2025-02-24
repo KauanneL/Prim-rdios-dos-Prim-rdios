5f2b5327d466e9ac61b22ca22817cb6cf1721d51
KauanneL <kauanne.l@escolar.ifrn.edu.br>
2025-02-24 07:21:58 -0300

    mod 1

--- src/controller/MedicoController.ts
+++ src/controller/MedicoController.ts
@@ -4,7 +4,7 @@ import * as MedicoModel from '../modelos/medicoModelo';
 // Função para obter os alunos
 export async function getMedicos(req: Request, res: Response) {
   try {
-    const medico = await MedicoModel.getMedicos();
+    const medico = await MedicoModel.getMedico();
     res.json(medico);
   } catch (error) {
     res.status(500).json({ message: 'Erro ao buscar médico' });
