cdb1a51d6271f7d155f04460c8afa5aab59f9a4a
KauanneL <kauanne.l@escolar.ifrn.edu.br>
2025-02-25 12:00:48 -0300

    +1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 async function fetchPacientes() {
-  const response = await axios.get("http://localhost:3000/api/pacientes");
+  const response = await axios.get("http://localhost:3000/api/paciente");
   return response.data;
 }
 
