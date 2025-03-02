fb9df253cfb5a5ecd061fe3e6850ce3513707271
Mariana <maristudys00@gmail.com>
2025-03-02 11:34:13 -0300

    ...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+  configurarFormularios();
   carregarPacientes();
   carregarMedicos();
   carregarSalas();
-  configurarFormularios();
 });
 
 async function carregarPacientes() {
