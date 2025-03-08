914c1f7308b61d78fe1779543458119ee4919f00
Mariana <maristudys00@gmail.com>
2025-03-08 18:04:46 -0300

    testando função

--- dist/main.js
+++ dist/main.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 export function formatarData(dataISO) {
     const data = new Date(dataISO);
-    return data.toLocaleDateString('pt-BR');
+    return data.toLocaleDateString('pt-BR', { timeZone: 'UTC' });
 }
 export function carregarPacientes() {
     return __awaiter(this, void 0, void 0, function* () {
