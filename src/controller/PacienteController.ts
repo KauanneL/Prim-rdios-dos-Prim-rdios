caf0679132ce219cb9c5125e6cf935a99ee20aee
KauanneL <137060290+KauanneL@users.noreply.github.com>
2025-03-09 21:59:32 -0300

    Update PacienteController.ts

--- src/controller/PacienteController.ts
+++ src/controller/PacienteController.ts
@@ -14,13 +14,12 @@ export async function criarPaciente(req: Request, res: Response): Promise<any> {
   const { nome, idade,telefone } = req.body;
 
   if (!nome || !idade || !telefone) {
-    return res.status(400).json({ message: 'Todos os campos devem ser preenchidos.' }); // Adiciona 'return'
+    return res.status(400).json({ message: 'Todos os campos devem ser preenchidos.' });
   }
 
   try {
     const result = await PacienteModel.criarPaciente(nome, idade,telefone);
-    return res.status(201).json({ id: result.insertId }); // Adiciona 'return' aqui também
-  } catch (error) {
-    return res.status(500).json({ message: 'Erro ao criar paciente' }); // 'return' aqui também
+    return res.status(201).json({ id: result.insertId }); 
+  } catch (error) {// 'return' aqui também
   }
 }
